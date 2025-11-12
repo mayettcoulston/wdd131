708a18cce511337ddf7128bc6d13e459db5e9deb
mayettcoulston <may20018@byui.edu>
2025-11-12 23:54:31 +0300

    Modification

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -2,28 +2,28 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('#button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-
-const deleteButton = document.createElement('button');
-
-li.textContent = input.value;
-
-deleteButton.textContent = 'X';
-
-li.append(deleteButton);
-
-list.append(li);
-
 button.addEventListener('click', function () {
-    if (input.value.trim() !== '') {
+    const chapter = input.value.trim();
+    if (chapter === '') {
+        alert('Please enter a favorite chapter!');
         input.focus();
+        return;
     }
-});
 
-deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = 'X';
+
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = '';
     input.focus();
-});
 
-input.value = '';
-input.focus();
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+});
